f0cbd0feebb81a46c70fb499a346352c4255b93e
bigtin <bigtin@proton.me>
2024-05-08 16:29:52 -0400

    added comments

--- main.cpp
+++ main.cpp
@@ -2,13 +2,17 @@
 
 int main(void)
 {
-    
+    //create window (size(x,y),window name)
     InitWindow(800, 450, "raylib [core] example - basic window");
 
+    //Main loop 
+    //without this the the winow would open and close instantly
     while (!WindowShouldClose())
     {
         BeginDrawing();
+            //backround colour
             ClearBackground(RAYWHITE);
+            //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
 
