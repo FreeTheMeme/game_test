4e4ad26e83cf42c01ed9202f87f96972e645c36b
bigtin <bigtin@proton.me>
2024-05-09 02:23:19 -0400

    add move

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include "raylib.h"
-
+#include <iostream>
+#define PLAYER_SIZE 40
 int main(void)
-{
+{   //vars
+    int width = 900;
+    int hieght = 650;
+    Rectangle player = {850, 600, PLAYER_SIZE, PLAYER_SIZE};
     //create window (size(x,y),window name)
-    InitWindow(800, 450, "raylib [core] example - basic window");
-
+    InitWindow(width, hieght, "raylib [core] example - basic window");
+    SetTargetFPS(60);
     //Main loop 
     //without this the the winow would open and close instantly
     while (!WindowShouldClose())
     {
         BeginDrawing();
             //backround colour
-            ClearBackground(RAYWHITE);
-            //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            ClearBackground(LIGHTGRAY);
+            if(IsKeyDown(KEY_A)) player.x -= 4.0f;
+            if(IsKeyDown(KEY_D)) player.x += 4.0f;
+            std::cout << "player" << player.x << std::endl;
+            DrawRectangleRec(player,ORANGE);
         EndDrawing();
     }
 
