d796e4f54aaeaf64c0e683591de6266a8eab02c7
bigtin <bigtin@proton.me>
2024-05-10 16:30:20 -0400

    camera works

--- main.cpp
+++ main.cpp
@@ -7,15 +7,27 @@
 #include "raymath.h"
 
 #define PLAYER_SIZE 48
+
 int main(void)
 {   //vars
-    int width = 1024;
-    int height = 1024;
-    Rectangle player = {(width/2)-24, (height/2)-24, PLAYER_SIZE, PLAYER_SIZE};
+    const int screenWidth = 1024;
+    const int screenHeight = 1024;
+    const Vector2 blockSize ={64,64};
+
+    Rectangle player = {(screenWidth/2), (screenHeight/2), PLAYER_SIZE, PLAYER_SIZE};
     float playerSpeed = 2.0f;
     //create window (size(x,y),window name)
-    InitWindow(width, height, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    //Camera
+    Camera2D camera = { 0 };
+    camera.zoom = 1.0f;
+    camera.target=  (Vector2){player.x,player.y};
+    camera.offset = (Vector2){ 512, 512 };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
     SetTargetFPS(60);
+
     //Main loop 
     //without this the the window would open and close instantly
     while (!WindowShouldClose())
@@ -24,28 +36,44 @@ int main(void)
             //background colour
             ClearBackground(LIGHTGRAY);
             //Key input
+            if(IsKeyDown(KEY_LEFT_SHIFT)) playerSpeed *= 2 ;
             if(IsKeyDown(KEY_W)) player.y-= playerSpeed;
             if(IsKeyDown(KEY_S)) player.y += playerSpeed;
             if(IsKeyDown(KEY_A)) player.x -= playerSpeed;
             if(IsKeyDown(KEY_D)) player.x += playerSpeed;
+            camera.target = (Vector2){ player.x + 20, player.y + 20 };
+
+        
+
+        // Camera zoom controls
+        camera.zoom += ((float)GetMouseWheelMove()*0.05f);
+
+        if (camera.zoom > 3.0f) camera.zoom = 3.0f;
+        else if (camera.zoom < 0.1f) camera.zoom = 0.1f;
+
+        // Camera reset (zoom and rotation)
+        if (IsKeyPressed(KEY_R))
+        {
+            camera.zoom = 1.0f;
+            camera.rotation = 0.0f;
+        }
             //Draws
-            std::cout << "player" << player.x << std::endl;
-            DrawFPS(16,16);
-            DrawText("pos: ",10,20,20,GREEN);
-            //grid
-            
-            
-            // Draw the 3d grid, rotated 90 degrees and centered around 0,0 
-            // just so we have something in the XY plane
-            rlPushMatrix();
-            rlTranslatef(0, 64*16, 0);
-            rlRotatef(90, 1, 0, 0);
-            DrawGrid(128, 64);
-            rlPopMatrix();
+            BeginMode2D(camera);
             DrawRectangleRec(player,ORANGE);
+                //Grid
+                //Draw the 3d grid, rotated 90 degrees and centered around 0,0 
+                //just so we have something in the XY plane
+                rlPushMatrix();
+                    rlTranslatef(0, 64*16, 0);
+                    rlRotatef(90, 1, 0, 0);
+                    DrawGrid(128, 64);
+                rlPopMatrix();
+                DrawRectangle(512,512,blockSize.x,blockSize.y,GRAY);
 
+            EndMode2D();
+            std::cout << "player x" << player.x << std::endl;
+            DrawFPS(16,16);
 
-            
         EndDrawing();
     }
 
