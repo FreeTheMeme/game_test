a17d7bf0c9f3bda461e3e30f82b44972dcf3579e
bigtin <bigtin@proton.me>
2024-05-10 15:02:36 -0400

    added grid

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include "raylib.h"
 #include <iostream>
-#define PLAYER_SIZE 40
+#include <string>
+#include "raylib.h"
+
+#include "rlgl.h"
+#include "raymath.h"
+
+#define PLAYER_SIZE 48
 int main(void)
 {   //vars
-    int width = 900;
-    int height = 650;
-    Rectangle player = {850, 600, PLAYER_SIZE, PLAYER_SIZE};
+    int width = 1024;
+    int height = 1024;
+    Rectangle player = {(width/2)-24, (height/2)-24, PLAYER_SIZE, PLAYER_SIZE};
     float playerSpeed = 2.0f;
     //create window (size(x,y),window name)
     InitWindow(width, height, "raylib [core] example - basic window");
     SetTargetFPS(60);
     //Main loop 
-    //without this the the winow would open and close instantly
+    //without this the the window would open and close instantly
     while (!WindowShouldClose())
     {
         BeginDrawing();
@@ -24,6 +30,20 @@ int main(void)
             if(IsKeyDown(KEY_D)) player.x += playerSpeed;
             //Draws
             std::cout << "player" << player.x << std::endl;
+            DrawFPS(16,16);
+            DrawText("pos: ",10,20,20,GREEN);
+            //grid
+            
+                
+            // Draw the 3d grid, rotated 90 degrees and centered around 0,0 
+            // just so we have something in the XY plane
+            rlPushMatrix();
+            rlTranslatef(0, 25*50, 0);
+            rlRotatef(90, 1, 0, 0);
+            DrawGrid(128, 46);
+            rlPopMatrix();
+
+            
             DrawRectangleRec(player,ORANGE);
         EndDrawing();
     }
