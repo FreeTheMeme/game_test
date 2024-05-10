1ebca20549afa938b112763786782bcde91a8339
bigtin <bigtin@proton.me>
2024-05-10 14:35:55 -0400

    Fixed spelling and added up and down.

--- main.cpp
+++ main.cpp
@@ -4,20 +4,25 @@
 int main(void)
 {   //vars
     int width = 900;
-    int hieght = 650;
+    int height = 650;
     Rectangle player = {850, 600, PLAYER_SIZE, PLAYER_SIZE};
+    float playerSpeed = 2.0f;
     //create window (size(x,y),window name)
-    InitWindow(width, hieght, "raylib [core] example - basic window");
+    InitWindow(width, height, "raylib [core] example - basic window");
     SetTargetFPS(60);
     //Main loop 
     //without this the the winow would open and close instantly
     while (!WindowShouldClose())
     {
         BeginDrawing();
-            //backround colour
+            //background colour
             ClearBackground(LIGHTGRAY);
-            if(IsKeyDown(KEY_A)) player.x -= 4.0f;
-            if(IsKeyDown(KEY_D)) player.x += 4.0f;
+            //Key input
+            if(IsKeyDown(KEY_W)) player.y-= playerSpeed;
+            if(IsKeyDown(KEY_S)) player.y += playerSpeed;
+            if(IsKeyDown(KEY_A)) player.x -= playerSpeed;
+            if(IsKeyDown(KEY_D)) player.x += playerSpeed;
+            //Draws
             std::cout << "player" << player.x << std::endl;
             DrawRectangleRec(player,ORANGE);
         EndDrawing();
